7e4709027cde7fb039b1bfeb0389d4a02d359cbb
Samir <bsam@bk.ru>
2022-02-03 23:37:27 +0300

    fix pages for pixel perfect

--- src/blocks/landing-page/landing-page.js
+++ src/blocks/landing-page/landing-page.js
@@ -1,4 +1,5 @@
 import './landing-page.scss';
 import './landing-page__background-img.scss';
 import './landing-page__find-room.scss';
+import './landing-page__info.scss';
 import './landing-page__find-room_margin-left_140px.scss';
