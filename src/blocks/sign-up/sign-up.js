0df3274de261da8817aa2a55ce694cd847b16de4
Samir <bsam@bk.ru>
2022-04-29 13:58:23 +0300

    add mask to date input #7

--- src/blocks/sign-up/sign-up.js
+++ src/blocks/sign-up/sign-up.js
@@ -1,1 +1,12 @@
+import IMask from 'imask';
 import './sign-up.scss';
+
+const elements = $('.input__input[data-class=date]');
+console.log([...elements]);
+const maskOptions = {
+  mask: Date,
+  pattern: 'd.`m.`Y',
+};
+[...elements].forEach((element) => {
+  (() => IMask(element, maskOptions))();
+});
