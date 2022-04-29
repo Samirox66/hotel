68975e71b7121a41797b1019df0437773996f4b3
Samir <bsam@bk.ru>
2022-04-29 23:25:49 +0300

    add moving to different picture while interacting with chooseing room block #7

--- src/blocks/choose-room/choose-room.js
+++ src/blocks/choose-room/choose-room.js
@@ -1,1 +1,55 @@
 import './choose-room.scss';
+import $ from 'jquery';
+
+$('.js-choose-room__img').on('mouseover', function () {
+  $(this)
+    .children('.choose-room__img-expand')
+    .removeClass('choose-room__img-expand_display_none');
+  $(this)
+    .children('.choose-room__dots')
+    .removeClass('choose-room__dots_display_none');
+});
+
+$('.js-choose-room__img').on('mouseleave', function () {
+  $(this)
+    .children('.choose-room__img-expand')
+    .addClass('choose-room__img-expand_display_none');
+  $(this)
+    .children('.choose-room__dots')
+    .addClass('choose-room__dots_display_none');
+});
+
+$('.js-choose-room__dot').on('click', function () {
+  const newImg = $(this).attr('data-class');
+  const $roomImg = $(this).parents('.choose-room__img');
+  $roomImg
+    .removeClass()
+    .addClass(`js-choose-room__img choose-room__img ${newImg}`);
+  $roomImg
+    .find('.choose-room__dot')
+    .removeClass('choose-room__dot_background_white');
+  $(this).addClass('choose-room__dot_background_white');
+});
+
+$('.js-choose-room__img-expand').on('click', function () {
+  const $roomImg = $(this).parents('.choose-room__img');
+  const currentImg = $(this).parent().attr('class').slice(37);
+  const dots = $(this).parent().find('.choose-room__dot');
+  const currentDot = [...dots].find(
+    (dot) => $(dot).attr('data-class') === currentImg
+  );
+  let index = [...dots].indexOf(currentDot);
+  $roomImg
+    .find('.choose-room__dot')
+    .removeClass('choose-room__dot_background_white');
+  if ($(this).children('.choose-room__expand-left').length) {
+    index = index - 1 < 0 ? 3 : index - 1;
+  } else {
+    index = index + 1 > 3 ? 0 : index + 1;
+  }
+  const newImg = $([...dots][index]).attr('data-class');
+  $roomImg
+    .removeClass()
+    .addClass(`js-choose-room__img choose-room__img ${newImg}`);
+  $([...dots][index]).addClass('choose-room__dot_background_white');
+});
--- src/blocks/sign-up/sign-up.js
+++ src/blocks/sign-up/sign-up.js
@@ -2,7 +2,6 @@ import IMask from 'imask';
 import './sign-up.scss';
 
 const elements = $('.input__input[data-class=date]');
-console.log([...elements]);
 const maskOptions = {
   mask: Date,
   pattern: 'd.`m.`Y',
